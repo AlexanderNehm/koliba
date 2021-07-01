4c459900d7d42e2831fa1a66939c12515fe39c24
Zamo437 <85157745+Zamo437@users.noreply.github.com>
2021-07-01 15:23:27 +0200

    update Anagram

--- AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -46,7 +46,9 @@ final class StaticWordLibrary extends WordLibrary {
         "Anna",
         "Regal",
         "Tragen",
-        "Mehl"
+        "Mehl",
+        "Nagel",
+        "Kiwi"
     };
 
     private static final String[] SCRAMBLED_WORD_LIST = {
@@ -60,7 +62,9 @@ final class StaticWordLibrary extends WordLibrary {
         "Anna",
         "Lager",
         "Garten",
-        "Helm"
+        "Helm",
+        "Algen",
+        "Wiki"
     };
     
     final static WordLibrary DEFAULT = new StaticWordLibrary();
